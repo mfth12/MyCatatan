de2295b1a21a897d964ae5f26b12b44acc8ec34f
Miftahul Haq <ciftah12@gmail.com>
2023-03-17 23:34:59 +0700

    final

--- src/Aplikasi.jsx
+++ src/Aplikasi.jsx
@@ -9,8 +9,8 @@ function Aplikasi() {
   const [query, setQuery] = useState("");
   const [cariCatatans, setCari] = useState([]);
   const [catatans, setCatatans] = useState(ambilDataCatatan());
-  const activeNotes = (cariCatatans || catatans).filter((note) => !note.archived);
-  const archivedNotes = (cariCatatans || catatans).filter((note) => note.archived);
+  const catatanAktif = (cariCatatans || catatans).filter((note) => !note.archived);
+  const catatanArsip = (cariCatatans || catatans).filter((note) => note.archived);
 
   useEffect(() => {
     setCari(catatans.filter((note) => note.title.toLowerCase().includes(query.toLowerCase())));
@@ -21,8 +21,8 @@ function Aplikasi() {
       <Header cari={query} updateQuery={setQuery} updateNotes={setCatatans} />
       <main className='main'>
         <InputForm updateCatatans={setCatatans} />
-        <BagianCatatan label='&#127894; Catatan Aktif' catatans={activeNotes} setCatatans={setCatatans} />
-        <BagianCatatan label='&#128194; Arsip' catatans={archivedNotes} setCatatans={setCatatans} />
+        <BagianCatatan label='&#127894; Catatan Aktif' catatans={catatanAktif} setCatatans={setCatatans} />
+        <BagianCatatan label='&#128194; Arsip' catatans={catatanArsip} setCatatans={setCatatans} />
       </main>
     </>
   );
